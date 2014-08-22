10c4a928edd5240c73a4e6bd02ca8e5c59377ebe
Sathyan Mathai <Sathyan.Mathai@gmail.com>
2014-08-22 11:29:00 -0400

    This commit redirects invalid selections, but it does not check to see if the url is already in the csv file.  It also does not display error messages or prepopulate the text fields.

--- server.rb
+++ server.rb
@@ -17,6 +17,49 @@ def pull_articles(filename)
   articles
 end
 
+def validation_results(name, url, description)
+  error_messages = []
+  description_test = description.gsub(" ", "")
+
+  if name.length == 0
+    error_messages << "The name field was left blank.  Please fill in the article name."
+  end
+
+  if url.length == 0
+    error_messages << "The url field was left blank.  Please fill in the url field."
+  end
+
+  if description_test.length > 20
+    error_messages << "The description field was greater than 20 characters.  Please input a description."
+  end
+
+  error_messages
+end
+
+
+# def valid_name?(name)
+
+#   if name.length == 0
+#     return false
+#   else
+#     return true
+#   end
+# end
+
+# def valid_url?(url)
+
+#   if url.length == 0
+#     return false
+#   end
+
+#   if
+
+
+
+# end
+
+
+
 
 ############################################
 #####                                 #####
@@ -37,6 +80,12 @@ post '/submit' do
   name = params["article_name"]
   url = params["article_url"]
   description = params["article_description"]
+  error_messages = validation_results(name, url, description)
+
+  if error_messages.length != 0
+
+    redirect "/submit"
+  end
 
   File.open('articles.csv','a') do |f|
     f.puts "#{name},#{url},#{description}"
